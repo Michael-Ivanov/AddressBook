3b40e1644d3c80aa4c10ad8c9197bd7531946ca2
Michael Ivanov <eesseth@gmail.com>
2021-05-04 20:57:10 +0300

    Add html table displaying list of persons

--- src/main/java/com/miv/controller/HomeController.java
+++ src/main/java/com/miv/controller/HomeController.java
@@ -1,15 +1,30 @@
 package com.miv.controller;
 
+import com.miv.entity.Person;
+import com.miv.service.PersonService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
 @RequestMapping("/")
-public class HelloController {
+public class HomeController {
+
+    private PersonService personService;
+
+    @Autowired
+    public HomeController(PersonService personService) {
+        this.personService = personService;
+    }
 
     @GetMapping
-    public String displayHomePage() {
+    public String displayHomePage(Model model) {
+        List<Person> personList = personService.getAll();
+        model.addAttribute(personList);
         return "home";
     }
 }
